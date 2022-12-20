f75366a649d916a5aa8ef145e9845ef69a5e4102
Ray Sylverne <raysylverne@gmail.com>
2022-12-20 17:21:02 +0000

    defining the difference between comments and multiline strings

--- rjs1_12_20_2022.py
+++ rjs1_12_20_2022.py
@@ -2,3 +2,13 @@
 
 print("Hello, World") # this is a trailing comments 
 
+"""
+This is not a block comment 
+This is a multiline string.
+This is an object that the compiler will have to go through and
+breakdown into bytecode that it's instantianting a new object
+Which will take up space in memory. So this is not free unlike 
+an actual comment. 
+"""
+# If you want to comment out multiple lines
+# You have to comment each line individually 
