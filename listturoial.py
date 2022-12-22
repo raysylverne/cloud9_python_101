7549d48bd65a20e6f53ba088078e4e9dd540aed6
Ray Sylverne <raysylverne@gmail.com>
2022-12-22 06:17:39 +0000

    LUIT Wk#12 Python Wk#1

--- listturoial.py
+++ listturoial.py
@@ -1,25 +1,33 @@
-'''
-Create a list of services using Python. IE: S3, Lambda, EC2, etc
+# 1. Create an empty list initially.
 
-1. The list should be empty initially.
-2. Populate the list using append or insert.
-3. Print the list and the length of the list.
-4. Remove two specific services from the list by name or by index.
-5. Print the new list and the new length of the list.
-'''
+aws_services = []
 
-# 1. The list should be empty initially.
+# 2.1 Populate the list using append
+aws_services.append('Cloud9')
+aws_services.append('CodeCommit')
+aws_services.append('CodeBuild')
 
-aws_services = ('a', 'b', 'c')
+# 2.2 Populate the list using insert.
+aws_services.insert(3, 'CodeDeploy')
 
-print("My Favorite AWS Services: ", aws_services[0])
+# 2.3 add mutiple AWS services to the list at once
+aws_services.extend(['CodePipeline', 'CloudFront', 'SQS', 'SNS', 'Lambda', 'EC2'])
 
-# 2. Populate the list using append or insert.
-
-aws_services + ['Lamdda', 'Cloud9', 'S3', 'EC2', 'CodeDeploy', 'CloudFront', 'CodePipeline', 'CodeBuild', 'CodeCommit']
 # 3. Print the list and the length of the list.
+print("Top 10 Used AWS Service: ")
+print(aws_services)
+print("Total # of most used services: ", len(aws_services))
+
 # 4. Remove two specific services from the list by name or by index.
+aws_services[6:] = []
+del aws_services[5]
+
 # 5. Print the new list and the new length of the list.
+print("Top 5 Used AWS Service: ")
+print(aws_services)
+print("Total # of most used services: ", len(aws_services))
+
+# Bonus How to srt a list 
+# sorted(aws_services)
 
 
-# create list
