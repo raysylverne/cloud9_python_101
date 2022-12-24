dc8b0ddefd8e9af376022becc340cc06b68a3a95
Ray Sylverne <raysylverne@gmail.com>
2022-12-24 01:44:21 +0000

    Separate the String and Present the Individual Words as a List

--- string_variations.py
+++ string_variations.py
@@ -1,2 +1,8 @@
 message = (input("Enter a Message: "))
 print("Lowercase:", message.lower())
+print("Uppercase: ", message.upper())
+print("Capitalized: ", message.capitalize())
+print("Title: ", message.title())
+
+seperate = message.split()
+print("Individual Words:", seperate)
