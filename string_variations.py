413d4fe44c1767bebea49ec9402840da536cfc06
Ray Sylverne <raysylverne@gmail.com>
2022-12-24 02:25:48 +0000

    Print the Alphabetic First and Last Words from the User's Input

--- string_variations.py
+++ string_variations.py
@@ -6,3 +6,7 @@ print("Title: ", message.title())
 
 seperate = message.split()
 print("Individual Words:", seperate)
+
+sorted_words = sorted(seperate)
+print("Alphabetic First Word:", sorted_words[0])
+print("Alphabetic Last Word:", sorted_words[-1])
