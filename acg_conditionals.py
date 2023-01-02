717306cfe2519163849c921e1a89f0d533186579
Ray Sylverne <raysylverne@gmail.com>
2023-01-02 21:14:20 +0000

    added a while loop so I don't have to keep running the app manually and added exit to allow user to leave upon completion

--- acg_conditionals.py
+++ acg_conditionals.py
@@ -1,8 +1,9 @@
-name = input( "What is your name: ")
-if len(name) >= 8:
-    print("Your name is to loog")
-elif len(name) == 7:
-    print(f"Your name is {name}")
-else:
-    print("Your name is too short!")
-
+name = ""
+while name != "exit":
+    name = input( "What is your name: ")
+    if len(name) >= 8:
+        print("Your name is to loog")
+    elif len(name) == 7:
+        print(f"Your name is {name}")
+    else:
+        print("Your name is too short!")
