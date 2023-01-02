b85bf1697988f2ae90a2b3f613616111001a6bc5
Ray Sylverne <raysylverne@gmail.com>
2023-01-02 21:20:38 +0000

    Seperated the user input from validate_and_excute function.

--- 8acg_conditionals.py
+++ 8acg_conditionals.py
@@ -1,9 +1,16 @@
+def validate_and_execute():
+    try: 
+    
+        if len(name) >= 8:
+            print("Your name is to loog")
+        elif len(name) == 7:
+            print(f"Your name is {name}")
+        else:
+            print("Your name is too short!")
+    except ValueError:
+        print("Your input is not a number don't crash the program")
+
 name = ""
 while name != "exit":
     name = input( "What is your name: ")
-    if len(name) >= 8:
-        print("Your name is to loog")
-    elif len(name) == 7:
-        print(f"Your name is {name}")
-    else:
-        print("Your name is too short!")
+    validate_and_execute()
