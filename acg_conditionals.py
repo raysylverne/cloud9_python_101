dadcf9e7da0da65c9c99f80e6397a81c89c05d7d
Ray Sylverne <raysylverne@gmail.com>
2023-01-02 21:11:08 +0000

    created a simple program to showcase if, elif, else conditionals

---
+++ acg_conditionals.py
@@ -0,0 +1,8 @@
+name = input( "What is your name: ")
+if len(name) >= 8:
+    print("Your name is to loog")
+elif len(name) == 7:
+    print(f"Your name is {name}")
+else:
+    print("Your name is too short!")
+
