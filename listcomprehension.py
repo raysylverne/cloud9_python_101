6d590d5c2a307c81e848c43cbc52c09cd35ad2ad
Ray Sylverne <raysylverne@gmail.com>
2023-01-05 22:06:00 +0000

    added more list comprehension examples

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,6 +1,32 @@
-colors = ['red', 'blue', 'orange', 'green', 'yellow']
-print(colors)
+colors = [ 'blue',  'green']
 uppercase_colors = []
-for test in colors
-    uppercase_colors.append('purple')
-    print(colors)
+for color in colors:
+    uppercase_colors.append(color.upper())
+#print(uppercase_colors)
+
+# Here's a different way to get the same outcome
+
+uppercase_colors = [color.upper() for color in colors]
+#print(uppercase_colors)
+
+
+'''
+Inside of a list comprehension the code to the left of the "for statement"
+Represents the expected outcome. Essentially that same as above with have 
+the lines of code. This is easier to read and great for filtering
+'''
+warm_colors = []
+for color in colors:
+    if color in ['red', 'orange', 'yellow']:
+        warm_colors.append(color)
+print(warm_colors)
+
+warm_colors = [ color for color in colors if color in ['red', 'orange', 'yellow']]
+print(warm_colors)
+
+
+# Just messing around to see what else can be done
+uppercase_colors = [color.replace('blue', 'pink') for color in colors]
+print(uppercase_colors)
+
+
